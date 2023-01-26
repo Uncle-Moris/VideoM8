083c484d37d06fe63b6d5ce5ae037d2251755f4d
maurycywoznica <mjw11@wp.pl>
2023-01-26 22:51:11 +0100

    abstract models

--- video/models.py
+++ video/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 
 
+class BasicModel(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Category(BasicModel):
+
+    class Meta:
+        ordering = ['name']
+
+
 class Video(models.Model):
     title = models.CharField(max_length=255, unique=True)
     #year
@@ -10,7 +29,7 @@ class Video(models.Model):
     #+ _rt
     #rating_imdb = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=5)
     description = models.TextField(max_length=1000, null=True)
-
+    categories = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.title
