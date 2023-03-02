0794a4914ec11b46ccfb885c038b23cae87268a6
maurycywoznica <mjw11@wp.pl>
2023-03-02 21:40:33 +0100

    factries for models

--- video/tests/factory.py
+++ video/tests/factory.py
@@ -13,7 +13,28 @@ from faker import Faker
 
 fake = Faker()
 
+
+class CategoryFactory(Factory):
+    class Meta:
+        model = Category
+
+    name = fake.name()
+
+class ActorFactory(Factory):
+    class Meta:
+        model = Actor
+
+    name = fake.name()
+
+
+class DirectorFactory(Factory):
+    class Meta:
+        model = Director
+
+    name = fake.name()
+
+
+
 class MovieFactory(Factory):
     class Meta:
         model = Movie
-
